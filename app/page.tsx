35636d318b50464ac54810e7daf7b35faa40361d
AJTimePyro <ajtimepyro@gmail.com>
2024-02-27 12:34:34 +0530

    added logo and favico

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import Navbar from "@/components/navbar";
+
 export default function Home() {
   return (
     <main>
-
+      <Navbar/>      
     </main>
   );
 }
---
+++ components/navbar.tsx
@@ -0,0 +1,17 @@
+export default function Navbar() {
+    return (
+        <nav className="flex justify-between bg-[#a9faa2]">
+            <div className="">
+                <img
+                    className="w-48 h-16 object-cover object-center"
+                    src="web-logo.png"
+                    alt="Virtual Meet Logo"
+                />
+            </div>
+
+            <div>
+
+            </div>
+        </nav>
+    );
+};
