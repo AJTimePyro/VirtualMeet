f0ae92014fc7d378faa8c7634b1fa77862ab50f0
AJTimePyro <ajtimepyro@gmail.com>
2024-03-11 22:59:09 +0530

    fixed key issue

--- lib/pusher.ts
+++ lib/pusher.ts
@@ -4,9 +4,9 @@ import PusherClient from "pusher-js";
 
 export const pusherServer = new PusherServer({
     appId : process.env.NEXT_PUSHER_APP_ID!,
-    key : process.env.NEXT_PUSHER_KEY!,
+    key : process.env.NEXT_PUBLIC_PUSHER_KEY!,
     secret : process.env.NEXT_PUSHER_SECRET!,
-    cluster : process.env.NEXT_PUSHER_CLUSTER!,
+    cluster : process.env.NEXT_PUBLIC_PUSHER_CLUSTER!,
     useTLS : true
 });
 
