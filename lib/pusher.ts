c901fc3d5ba6818d9ac32e22f2621bbf5bb79d2d
AJTimePyro <ajtimepyro@gmail.com>
2024-03-11 22:21:35 +0530

    changed env variables names for pusher to make it safe

--- lib/pusher.ts
+++ lib/pusher.ts
@@ -3,16 +3,16 @@ import PusherClient from "pusher-js";
 
 
 export const pusherServer = new PusherServer({
-    appId : process.env.NEXT_PUBLIC_PUSHER_APP_ID!,
-    key : process.env.NEXT_PUBLIC_PUSHER_KEY!,
-    secret : process.env.NEXT_PUBLIC_PUSHER_SECRET!,
-    cluster : process.env.NEXT_PUBLIC_PUSHER_CLUSTER!,
+    appId : process.env.NEXT_PUSHER_APP_ID!,
+    key : process.env.NEXT_PUSHER_KEY!,
+    secret : process.env.NEXT_PUSHER_SECRET!,
+    cluster : process.env.NEXT_PUSHER_CLUSTER!,
     useTLS : true
 });
 
 export const pusherClient = new PusherClient(
-    process.env.NEXT_PUBLIC_PUSHER_KEY!,
+    process.env.NEXT_PUSHER_KEY!,
     {
-        cluster : process.env.NEXT_PUBLIC_PUSHER_CLUSTER!
+        cluster : process.env.NEXT_PUSHER_CLUSTER!
     }
 );
