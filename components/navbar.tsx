aedcc2d60ccd34efd9b9fa51ac14a39f1e40ff28
AJTimePyro <ajtimepyro@gmail.com>
2024-03-15 10:25:45 +0530

    added homepage link to logo in navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,15 +1,16 @@
+import Link from "next/link";
 import RealTime from "./realTime";
 
 export default function Navbar() {
     return (
         <nav className="flex justify-between bg-[#a9faa2] pl-4 pr-4 items-center">
-            <div>
+            <Link href="/">
                 <img
                     className="w-48 h-16 object-cover object-center"
                     src="/web-logo.png"
                     alt="Virtual Meet Logo"
                 />
-            </div>
+            </Link>
 
             <RealTime />
         </nav>
